41690044d4f2293d36ecab4914b4499277413dda
jungeuidon <mantopuma@naver.com>
2019-08-07 18:28:22 +0900

    4번완료

--- src/prob04/Prob04.java
+++ src/prob04/Prob04.java
@@ -10,11 +10,23 @@ public class Prob04 {
 	}
 	
 	public static char[] reverse(String str) {
+		char c1[] = new char[str.length()];
 		/* 코드를 완성합니다 */
-		return null;
+		for (int i = 0; i < str.length(); i++) {
+			c1[i]=str.charAt(str.length()-1-i);
+//			for (int j=str.length()-1;  j>=0 ; j--) {
+//				
+//			}
+		}
+			
+		System.out.println();
+		return c1;
 	}
 
 	public static void printCharArray(char[] array){
 		/* 코드를 완성합니다 */
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+		}
 	}
 }
